151f23ceb150431f2ee9bcff36a2490a582c58e7
Evpebe <evpebe@floridauniversitaria.es>
2023-02-10 17:51:52 +0100

    guardado archivos

--- b.sh
+++ b.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 matriculas=$((`cat notas.txt | wc -l | head -n2`))
-<<<<<<< HEAD
 echo "Hay un total de $matriculas matriculas"
-=======
 echo "Hay un total de $matriculas matriculas"
->>>>>>> parte2
